a2a17baf11284d34e6ef722039ab9a94c03efbef
Harald Schlieder <harald.schlieder1@gmail.com>
2024-06-30 20:59:45 +0200

    dd

--- node01/src/main.rs
+++ node01/src/main.rs
@@ -110,6 +110,7 @@ async fn server(addr: SocketAddrV4) -> io::Result<()> {
     let rdma = rdma_listener.accept(1, 1, 512).await?;
     // run here after client connect
     let lmr = rdma.receive_local_mr().await?;
+    println("Debug Server: {:?}", lmr.as_slice());
     let lmr_contant = lmr.as_slice().read_line()?;
     println!("Server received: {:?}", lmr_contant);
     Ok(())
