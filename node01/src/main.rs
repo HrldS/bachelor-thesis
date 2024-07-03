c3995be68b0d5c8184c29aba0c55e2f15362e392
Harald Schlieder <harald.schlieder1@gmail.com>
2024-07-03 20:21:16 +0200

    d

--- node01/src/main.rs
+++ node01/src/main.rs
@@ -112,7 +112,7 @@ async fn client_rdma(addr: SocketAddrV4, rdma_type: &str) -> io::Result<()> {
 }
 
 fn client_tcp() -> io::Result<()> {
-    let mut stream = TcpStream::connect("192.168.100.52:41000");
+    let mut stream = TcpStream::connect("192.168.100.52:41000")?;
 
     let file = File::open("src/data/test_data.csv")?;  //? try reading file
     let mut content = csv::ReaderBuilder::new().has_headers(false).delimiter(b';').from_reader(file); // Disable headers assumption to not skip first row
