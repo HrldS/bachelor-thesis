ecbf39a77178bd19694e26acbddac85ca5cd230a
Harald Schlieder <harald.schlieder1@gmail.com>
2024-07-04 22:14:49 +0200

    d

--- node01/src/main.rs
+++ node01/src/main.rs
@@ -218,7 +218,7 @@ fn client_tcp(size: &str) -> io::Result<()> {
     let data = data_formating(size);
 
     for line in data {
-        let message = line;
+        let message = line.as_str();
         stream.write_all(message.as_bytes())?;
         stream.flush()?;
     }
