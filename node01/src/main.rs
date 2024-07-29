189540ca8882d488e686f5838cd5c130a583694f
Harald Schlieder <harald.schlieder1@gmail.com>
2024-07-29 18:00:41 +0200

    d

--- node01/src/main.rs
+++ node01/src/main.rs
@@ -103,7 +103,7 @@ async fn client_rdma(addr: &str, rdma_type: &str, size: &str) -> io::Result<()> 
 
     if rdma_type == "write" {
 
-        let layout = Layout::from_value(&file_size);
+        let layout = Layout::for_value(&file_size);
 
         let mut lmr = rdma.alloc_local_mr(layout)?;
         let mut rmr = rdma.request_remote_mr(layout).await?;
