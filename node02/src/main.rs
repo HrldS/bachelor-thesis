aba2a971048f88ab957811bacc9a87139d69cb8c
Harald Schlieder <harald.schlieder1@gmail.com>
2024-07-29 13:55:20 +0200

    clean

--- node02/src/main.rs
+++ node02/src/main.rs
@@ -1,10 +1,6 @@
 extern crate csv;
 extern crate tokio;
-/*
-use std::io::{self, BufRead};
-use std::net::{TcpListener, TcpStream}; // Ipv4Addr, SocketAddrV4
-//use portpicker::pick_unused_port;
-*/
+
 use std::error::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener,TcpStream};
@@ -16,57 +12,43 @@ async fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     println!("first line");
 
     let mut data_buffer = Vec::new();
-
-    stream.read_to_end(&mut data_buffer).await?;        //write the data from the stream into the data_buffer
+    stream.read_to_end(&mut data_buffer).await?;  // Write the data from the stream into the data_buffer
     println!("after read_to_end");
-    let mut reader = csv::ReaderBuilder::new().has_headers(false).from_reader(data_buffer.as_slice());  //read the data from the data_buffer
 
-    let mut message_buffer = Writer::from_writer(Vec::new()); //create the buffer to write the processed records into
+    let processed_data = process_data(data_buffer)?;  // Process the data
+    println!("Called");
 
-    for content in reader.records() {  // calculate the volume for each record in the received csv file
+    stream.write_all(&processed_data).await?; // Send the processed message bytes back to client
+    stream.flush().await?; // Ensure that the entire message is sent
+    stream.shutdown().await?;
+    Ok(())
+}
+
+fn process_data(data_buffer: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
+    let mut reader = ReaderBuilder::new().has_headers(false).from_reader(data_buffer.as_slice());  // Read the data from the data_buffer
+    let mut message_buffer = Writer::from_writer(Vec::new()); // Create the buffer to write the processed records into
+
+    for content in reader.records() {  // Calculate the volume for each record in the received CSV file
         let record = content?;
 
-        // get the 3 necessary values from the csv record to calculate the volume
+        // Get the 3 necessary values from the CSV record to calculate the volume
         let col1: i32 = record[1].parse()?;     
         let col2: i32 = record[2].parse()?;
         let col3: i32 = record[3].parse()?;
         let object_volume = col1 * col2 * col3;
         
-        let mut new_record = record.clone();    // take the original record
-        new_record.push_field(&object_volume.to_string()); // create the processed record by adding the volume to the original record
+        let mut new_record = record.clone();  // Take the original record
+        new_record.push_field(&object_volume.to_string()); // Create the processed record by adding the volume to the original record
         
-        message_buffer.write_record(&new_record)?; //write the processed records into the message buffer
+        message_buffer.write_record(&new_record)?; // Write the processed records into the message buffer
     }
 
-    let send_message = message_buffer.into_inner()?; // return the bytes of the processed record
-    println!("Called");
-    stream.write_all(&send_message).await?; //send the processed message bytes back to client
-
-    stream.flush().await?; // ensure that the entire message is send test
-
-    stream.shutdown().await?;
-    Ok(())
+    let send_message = message_buffer.into_inner()?; // Return the bytes of the processed record
+    Ok(send_message)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    /*
-    let listener = TcpListener::bind("192.168.100.52:41000")?;
-    let local_addr = listener.local_addr()?;
-    println!("Server listening on: {}", local_addr);
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-               std::thread::spawn(|| handle_client(stream));
-            }
-            Err(e) => {
-                eprintln!("Connection failed: {}", e);
-            }
-        }
-    }
-    Ok(())
-    */
     
     let mut server_type = String::new();
     loop {
@@ -94,7 +76,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         while let Ok((stream, _)) = listener.accept().await {
             println!("New connection: {:?}", stream.peer_addr());  //print incoming client ip address
                     
-                    // Spawn a new tokio task to handle each client
+            // Spawn a new tokio task to handle each client
             tokio::spawn(async move {
                 println!("inside tokyo spawn");
                 if let Err(err) = handle_client(stream).await {
@@ -107,23 +89,3 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("The Transportation protocol: {:?} choosen!", server_type);
     Ok(())
 }
-
-    /*
-    handle_client:
-    let reader = io::BufReader::new(stream);
-    let mut count = 1;
-    for line in reader.lines() {
-        match line {
-            Ok(line) => {
-                println!("Received: {:?}", line);
-                println!("Called times {}", count);
-                count += 1;
-            }
-            Err(e) => {
-                eprintln!("Error reading from client: {}", e);
-                break;
-            }
-        }
-    }
-    Ok(())
-    */
