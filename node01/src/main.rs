c809f480d5a0445d2147c1f4a330b22081f020f8
Harald Schlieder <harald.schlieder1@gmail.com>
2024-07-29 18:20:10 +0200

    d

--- node01/src/main.rs
+++ node01/src/main.rs
@@ -111,11 +111,7 @@ async fn client_rdma(addr: &str, rdma_type: &str, size: &str) -> io::Result<()> 
         println!("Debug: Client about to write {:?}", file_data);
         println!();
 
-        { 
-        //let _num = lmr.as_mut_slice().write(&file_data)?;
-            let mut lmr_slice = lmr.as_mut_slice();
-            lmr_slice[..file_size].copy_from_slice(&file_data[..file_size]);
-        }
+        let _num = lmr.as_mut_slice().write(&file_data)?;
 
         rdma.write(&lmr, &mut rmr).await?;
 
