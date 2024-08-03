61eb507ff6ab2948b2c3edca2c6b038be002d8b7
Harald Schlieder <harald.schlieder1@gmail.com>
2024-08-03 17:15:40 +0200

    d

--- node02/src/main.rs
+++ node02/src/main.rs
@@ -29,9 +29,13 @@ async fn tcp_handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> 
 }
 
 async fn rdma_send_handle_client(addr: String) -> Result<(), Box<dyn std::error::Error>> {
-    let rdma = RdmaBuilder::default().with_buffer_size(21 * 1048576).listen(&addr).await?;
+    let rdma = RdmaBuilder::default().listen(&addr).await?;
 
-    let message = rdma.receive().await?;
+    let _rdma_memory_increase = rdma.alloc_local_mr();
+    
+    let layout = Layout::from_size_align(21 * 1048576, std::mem::align_of::<u8>()).expect("Failed to create layout");
+
+    let message = rdma.receive(layout).await?;
     let message_contents = message.as_slice().to_vec();
 
     println!("Received data: {} bytes", message_contents.len());
