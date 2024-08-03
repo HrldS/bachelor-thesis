7708f95d7f6ef23d58d6ad8e01d564dd41c771f3
Harald Schlieder <harald.schlieder1@gmail.com>
2024-08-03 17:34:47 +0200

    d

--- node02/src/main.rs
+++ node02/src/main.rs
@@ -30,7 +30,8 @@ async fn tcp_handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> 
 
 async fn rdma_send_handle_client(addr: String) -> Result<(), Box<dyn std::error::Error>> {
     let rdma_listener = RdmaListener::bind(addr).await?;
-    let rdma = rdma_listener.accept(1, 1,21 * 1048576).await?;
+    let size = 100;
+    let rdma = rdma_listener.accept(1, 1,size).await?;
 
     let message = rdma.receive().await?;
     let message_contents = message.as_slice().to_vec();
