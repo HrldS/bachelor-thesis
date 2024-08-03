4ba2f177c0e6809209bf412b9db95a359059b6a5
Harald Schlieder <harald.schlieder1@gmail.com>
2024-08-03 16:24:57 +0200

    d

--- node02/src/main.rs
+++ node02/src/main.rs
@@ -33,6 +33,7 @@ async fn rdma_send_handle_client(addr: String) -> Result<(), Box<dyn std::error:
 
     let message = rdma.receive().await?;
     let message_contents = message.as_slice().to_vec();
+    println!("Received data: {} bytes", message_contents.len());
     println!("rdy for process");
     let processed_data = match process_data(message_contents) {
         Ok(data) => data,
